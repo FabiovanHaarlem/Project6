b92a0c020999cc6bcbe2045e34c351befca3eb91
Kyle van den Boom <520163@student.glu.nl>
2017-11-20 09:56:23 +0100

    Added Units And UnitEditor

--- Project Periode 6 - Ronimo/Assets/Editor/UnitEditor.cs
+++ Project Periode 6 - Ronimo/Assets/Editor/UnitEditor.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
-[CustomEditor(typeof()]
+[CustomEditor(typeof(Unit))]
 public class UnitEditor : Editor
 {
-    
+    public override void OnInspectorGUI()
+    {
+        GUILayout.Label("");
+    }
 }
