6a701d8fd21712396489efb82adabe5011da004e
Fabio van Haarlem <fabio.van.haarlem@hotmail.nl>
2017-11-20 12:45:37 +0100

    Added Select, Pool and Spawn systems

---
+++ Project Periode 6 - Ronimo/Assets/Scripts/Fabio/UnitPool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPool : MonoBehaviour
+{
+    private int m_AmountOfUnits;
+    private List<GameObject> m_UnitPool;
+	
+	void Start ()
+    {
+        GetAllUnitPrefabs();
+	}
+
+    private void GetAllUnitPrefabs()
+    {
+        for (int i = 0; i < m_AmountOfUnits; i++)
+        {
+            m_UnitPool.Add(Instantiate(Resources.Load<GameObject>("Prefabs/CollecterUnitTest")));
+            m_UnitPool.Add(Instantiate(Resources.Load<GameObject>("Prefabs/MeleeUnitTest")));
+            m_UnitPool.Add(Instantiate(Resources.Load<GameObject>("Prefabs/RangedUnitTest")));
+            m_UnitPool.Add(Instantiate(Resources.Load<GameObject>("Prefabs/SpellCasterUnitTest")));
+        }
+
+        for (int i = 0; i < m_UnitPool.Count; i++)
+        {
+            m_UnitPool[i].SetActive(false);
+        }
+    }
+
+    public GameObject GetSelectedUnit(string unitName)
+    {
+        GameObject unit = null; ;
+
+        for (int i = 0; i < m_UnitPool.Count; i++)
+        {
+            if (m_UnitPool[i].name == unitName)
+            {
+                if (!m_UnitPool[i].activeInHierarchy)
+                {
+                    unit = m_UnitPool[i];
+                }
+            }
+        }
+
+        return unit;
+    }
+	
+}
---
+++ Project Periode 6 - Ronimo/Assets/Scripts/Fabio/UnitSelectManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectManager : MonoBehaviour
+{
+    private UnitPool m_UnitPool;
+    private UnitSpawnerManager m_Spawner;
+
+	void Start ()
+    {
+        m_UnitPool = GetComponent<UnitPool>();
+        m_Spawner = GetComponent<UnitSpawnerManager>();
+	}
+	
+    public void SpawnUnit(string unitName)
+    {
+        GameObject unit = m_UnitPool.GetSelectedUnit(unitName);
+        m_Spawner.SpawnUnit(unit);
+    }
+
+    public void SetUIElements()
+    {
+        Vector3 mousePosition = 
+    }
+}
