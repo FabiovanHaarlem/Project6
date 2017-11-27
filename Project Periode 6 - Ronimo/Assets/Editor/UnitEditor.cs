b77d30eecf4cbe2d39c5c3adea6a2450e8d39013
Kyle van den Boom <520163@student.glu.nl>
2017-11-27 12:28:52 +0100

    The unit editor can now spawn units with sprites, health, damage, movespeed, a type, a range and a gold cost

--- Project Periode 6 - Ronimo/Assets/Editor/UnitEditor.cs
+++ Project Periode 6 - Ronimo/Assets/Editor/UnitEditor.cs
@@ -3,69 +3,98 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(Unit))]
+[CustomEditor(typeof(UnitMaker))]
 public class UnitEditor : Editor
 {
     //Melee = 0, Ranged = 1;
-    string[] m_unitrange = new string[] { "Melee", "Ranged" };
-    string[] m_meleetypes = new string[] { "Collecter", "Swordmaster" };
-    string[] m_rangedtypes = new string[] { "Spellcaster", "Archer" };
+    //string[] m_unitrange = new string[] { "Melee", "Ranged" };
+    //string[] m_meleetypes = new string[] { "Collecter", "Swordmaster" };
+    //string[] m_rangedtypes = new string[] { "Spellcaster", "Archer" };
+
+    string[] m_unitypes = new string[] { "Collector", "Swordmaster", "Spellcaster", "Archer" };
+    string[] m_rangetype = new string[] { "Melee", "Ranged" };
+
+    int m_healthamount;
+    int m_damageamount;
+    float m_movespeed;
+    int m_goldamount;
 
     string m_prefabname = "";
 
+    //0 = collector, 1 = swordmaster, 2 = spellcaster, 3 = archer
+    int m_unittypechoice = 0;
+    //0 = melee, 1 = ranged
     int m_unitrangechoice = 0;
-    int m_unitmeleetypechoice = 0;
-    int m_unitrangetypechoice = 0;
-    Unit m_unitscript;
+
+    UnitMaker m_unitscript;
+
+    Sprite m_unitsprite;
 
     void OnEnable()
     {
-        m_unitscript = (Unit)target;
+        m_unitscript = (UnitMaker)target;
     }
 
     [MenuItem("Editor/Unit Editor")]
-
     public override void OnInspectorGUI()
     {
         //Draw The Default Inspector
         DrawDefaultInspector();
-        EditorGUILayout.LabelField("Unit Range");
-        m_unitrangechoice = EditorGUILayout.Popup(m_unitrangechoice, m_unitrange);
-        Unit unit = target as Unit;
-        //Update The Selected Choice
-        unit.SetUnitChoice(m_unitrange[m_unitrangechoice]);
+        UnitMaker unit = target as UnitMaker;
 
-        switch (m_unitrangechoice)
-        {
-            //Melee
-            case 0:
-                EditorGUILayout.LabelField("Melee Type");
-                m_unitmeleetypechoice = EditorGUILayout.Popup(m_unitmeleetypechoice, m_meleetypes);
-                unit.SetUnitType(m_meleetypes[m_unitmeleetypechoice]);
-                EditorUtility.SetDirty(target);
-                break;
+        EditorGUILayout.LabelField("Unit Sprite");
+        m_unitsprite = (Sprite)EditorGUILayout.ObjectField(m_unitsprite, typeof(Sprite), false);
 
-            //Ranged
-            case 1:
-                EditorGUILayout.LabelField("Ranged Type");
-                m_unitrangetypechoice = EditorGUILayout.Popup(m_unitrangetypechoice, m_rangedtypes);
-                unit.SetUnitType(m_rangedtypes[m_unitrangetypechoice]);
-                EditorUtility.SetDirty(target);
-                break;
-        }
+        EditorGUILayout.LabelField("Unit Type");
+        m_unittypechoice = EditorGUILayout.Popup(m_unittypechoice, m_unitypes);
+
+        EditorGUILayout.LabelField("Range Type");
+        m_unitrangechoice = EditorGUILayout.Popup(m_unitrangechoice, m_rangetype);
+
+        EditorGUILayout.LabelField("Amount Of Damage");
+        m_damageamount = EditorGUILayout.IntSlider(m_damageamount, 0, 500);
+        ProgressBar(m_damageamount / 500.0f, "Damage");
+
+        EditorGUILayout.LabelField("Amount Of Health");
+        m_healthamount = EditorGUILayout.IntSlider(m_healthamount, 0, 1000);
+        ProgressBar(m_healthamount / 1000.0f, "Health");
+
+        EditorGUILayout.LabelField("Movement Speed");
+        m_movespeed = EditorGUILayout.FloatField(m_movespeed);
+
+        EditorGUILayout.LabelField("Amount Of Gold");
+        m_goldamount = EditorGUILayout.IntSlider(m_goldamount, 0, 2000);
 
         EditorGUILayout.LabelField("Prefab Name");
-        m_prefabname = EditorGUILayout.TextField(m_prefabname);
+        m_prefabname = EditorGUILayout.TextField(m_prefabname);     
+
+        EditorUtility.SetDirty(target);
 
         if (GUILayout.Button("Generate Unit"))
         {
             GameObject newunit = new GameObject();
             newunit.AddComponent<UnitStats>();
-            //newunit.GetComponent<UnitStats>().m_rangetype = (UnitStats.RangeType)m_unitrangechoice;
-            //newunit.GetComponent<UnitStats>().m_meleeunittype = (UnitStats.MeleeUnitType)m_unitmeleetypechoice;
-            //newunit.GetComponent<UnitStats>().m_rangedunittype = (UnitStats.RangedUnitType)m_unitrangetypechoice;
+            newunit.AddComponent<SpriteRenderer>();
+
+            newunit.GetComponent<UnitStats>().SetUnit((UnitStats.UnitType)m_unittypechoice);
+            newunit.GetComponent<UnitStats>().SetRange((UnitStats.UnitRange)m_unitrangechoice);
+            newunit.GetComponent<UnitStats>().SetDamage(m_damageamount);
+            newunit.GetComponent<UnitStats>().SetHealth(m_healthamount);
+            newunit.GetComponent<UnitStats>().SetMovespeed(m_movespeed);
+            newunit.GetComponent<UnitStats>().SetGoldCost(m_goldamount);
+            newunit.GetComponent<SpriteRenderer>().sprite = m_unitsprite;
+
+            newunit.name = m_prefabname;
 
             PrefabUtility.CreatePrefab("Assets/Resources/Prefabs/UnitPrefabs/" + m_prefabname + ".prefab", newunit);
         }
     }
+
+    void ProgressBar(float value, string label)
+    {
+        Rect rectangle = GUILayoutUtility.GetRect(18, 18, "TextField");
+        EditorGUI.ProgressBar(rectangle, value, label);
+        EditorGUILayout.Space();
+    }
 }
+
--- Project Periode 6 - Ronimo/Assets/Scripts/Kyle/UnitStats.cs
+++ Project Periode 6 - Ronimo/Assets/Scripts/Kyle/UnitStats.cs
@@ -4,28 +4,85 @@ using UnityEngine;
 
 public class UnitStats : MonoBehaviour
 {
-    public enum RangeType
+    public enum UnitRange
     {
         Melee,
         Ranged
     };
 
-    public enum MeleeUnitType
+    public enum UnitType
     {
         Collector,
-        SwordMaster
-    };
-
-    public enum RangedUnitType
-    {
+        SwordMaster,
         Spellcaster,
-        Archer
+        Archer,
+        //SpecialUnit,
     };
 
+    private UnitRange m_unitrange;
+    private UnitType m_unittype;
+    int m_health;
+    int m_damage;
+    float m_movespeed;
+    int m_goldcost;
 
-    private RangeType m_rangetype;
-    private MeleeUnitType m_meleeunittype;
-    private RangedUnitType m_rangedunittype;
+    public void SetRange(UnitRange range)
+    {
+        m_unitrange = range;
+    }
 
-   // void SetMeleeUnitType
+    public UnitRange GetUnitRange()
+    {
+        return m_unitrange;
+    }
+
+    public void SetUnit(UnitType type)
+    {
+        m_unittype = type;
+    }
+
+    public UnitType GetUnitType()
+    {
+        return m_unittype;
+    }
+
+    public void SetHealth(int amount)
+    {
+        m_health = amount;
+    }
+
+    public int GetUnitHealth()
+    {
+        return m_health;
+    }
+
+    public void SetDamage(int amount)
+    {
+        m_damage = amount;
+    }
+
+    public int GetUnitDamage()
+    {
+        return m_damage;
+    }
+
+    public void SetMovespeed(float amount)
+    {
+        m_movespeed = amount;
+    }
+
+    public float GetMovespeed()
+    {
+        return m_movespeed;
+    }
+
+    public void SetGoldCost(int amount)
+    {
+        m_goldcost = amount;
+    }
+
+    public int GetGoldCost()
+    {
+        return m_goldcost;
+    }
 }
